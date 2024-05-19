f31b40c8c6805a20fdd4dc0026a6a40e09cf0820
phamngocthi <pnthi.it.cp@gmail.com>
2024-05-19 20:12:06 +0700

    check test

--- train.py
+++ train.py
@@ -113,6 +113,7 @@ def train(config):
             optimizer.zero_grad(set_to_none=True)
 
             global_step += 1
+            break
 
         # val
         with torch.no_grad():
@@ -137,6 +138,7 @@ def train(config):
                 sum_loss_val += loss.item()
                 losses_val_step.append(loss.item())
                 batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+                break
 
         losses_train.append(sum_loss_train / len(train_dataloader))
         losses_val.append(sum_loss_val / len(val_dataloader))
