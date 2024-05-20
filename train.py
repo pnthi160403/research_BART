4af0831268fdf6d86fd44c958c9e03dd8421a4cd
phamngocthi <pnthi.it.cp@gmail.com>
2024-05-20 09:04:38 +0700

    test input

--- train.py
+++ train.py
@@ -112,6 +112,10 @@ def train(config):
             src_attention_mask = (src != tokenizer_src.token_to_id("<pad>")).type(torch.int64)
             tgt_attention_mask = (tgt != tokenizer_tgt.token_to_id("<pad>")).type(torch.int64)
             label = batch['label'].to(device)
+
+            print("src: ", src[0])
+            print("tgt: ", tgt[0])
+            print("label: ", label[0])
             
             logits = model(
                 input_ids=src,
