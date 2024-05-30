c7695f564ae988a3dda684c22d6a1d59bd652967
phamngocthi <pnthi.it.cp@gmail.com>
2024-05-30 14:53:11 +0700

    fix bug successfully

--- train.py
+++ train.py
@@ -225,8 +225,8 @@ def train(config):
                         
                         global_val_step += 1
                         # debug
-                        if global_step == 10:
-                            break
+                        # if global_step == 10:
+                        #     break
 
                     if global_step % config["val_steps"] == 0:
                         losses_train.append(sum_loss_train / len(train_dataloader))
@@ -240,11 +240,11 @@ def train(config):
                 break
 
             # debug
-            if global_step == 10:
-                break
+            # if global_step == 10:
+                # break
         # debug
-        if global_step == 10:
-            break
+        # if global_step == 10:
+            # break
 
     # save model
     save_model(
