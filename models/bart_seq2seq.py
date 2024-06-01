f24e53049bbc7a7bbfbd171224da90b7861bc39a
phamngocthi <pnthi.it.cp@gmail.com>
2024-06-01 11:30:03 +0700

    update init weight

--- models/bart_seq2seq.py
+++ models/bart_seq2seq.py
@@ -100,23 +100,23 @@ class BartSeq2seq(nn.Module):
         return logits
     
     def _init_weights(self, module, mean=0.0, std=0.02, init_type="normal"):
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=mean, std=std)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=mean, std=std)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
+        # if isinstance(module, nn.Linear):
+        #     module.weight.data.normal_(mean=mean, std=std)
+        #     if module.bias is not None:
+        #         module.bias.data.zero_()
+        # elif isinstance(module, nn.Embedding):
+        #     module.weight.data.normal_(mean=mean, std=std)
+        #     if module.padding_idx is not None:
+        #         module.weight.data[module.padding_idx].zero_()
 
-        # for param in module.parameters():
-        #     if param.dim() > 1:
-        #         if init_type == "normal":
-        #             nn.init.normal_(param, mean=mean, std=std)
-        #         elif init_type == "xavier":
-        #             nn.init.xavier_normal_(param)
-        #         else:
-        #             continue
+        for param in module.parameters():
+            if param.dim() > 1:
+                if init_type == "normal":
+                    nn.init.normal_(param, mean=mean, std=std)
+                elif init_type == "xavier":
+                    nn.init.xavier_normal_(param)
+                else:
+                    continue
     
     def initialize_weights(self, modules, init_type="normal", mean=0.0, std=0.02):
         for module in modules:
