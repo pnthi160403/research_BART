be2748f3ceb2c902df959b370fafcb6e38a426af
phamngocthi <pnthi.it.cp@gmail.com>
2024-06-03 08:56:22 +0700

    update

--- models/bart_model_from_scratch/multihead_attn.py
+++ models/bart_model_from_scratch/multihead_attn.py
@@ -30,6 +30,17 @@ class BartAttention(nn.Module):
     ):
         return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
     
+    @staticmethod
+    def _sdpa(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, float("-inf"))
+        attention_scores = attention_scores.softmax(dim=-1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        return (attention_scores @ value)
+    
     def forward(
         self,
         hidden_states: torch.Tensor,
@@ -37,47 +48,26 @@ class BartAttention(nn.Module):
         attention_mask: torch.Tensor=None,
         layer_head_mask: torch.Tensor=None,
     ):
-        bsz, tgt_len, _ = hidden_states.size()
-        proj_shape = (bsz * self.num_heads, -1, self.head_dim)
-
-        query_states = self.q_proj(hidden_states) * self.scaling
-        query_states = self._shape(query_states, tgt_len, bsz).view(*proj_shape)
-
+        query_states = self.q_proj(hidden_states)
         if key_value_states is None:
-            key_states = self._shape(self.k_proj(hidden_states), -1, bsz)
-            value_states = self._shape(self.v_proj(hidden_states), -1, bsz)
+            key_states = self.k_proj(hidden_states)
+            value_states = self.v_proj(hidden_states)
         else: # is cross-attention
-            key_states = self._shape(self.k_proj(key_value_states), -1, bsz)
-            value_states = self._shape(self.v_proj(key_value_states), -1, bsz)            
+            key_states = self.k_proj(key_value_states)
+            value_states = self.v_proj(key_value_states)
 
-        key_states = key_states.reshape(*proj_shape)
-        value_states = value_states.reshape(*proj_shape)
-
-        src_len = key_states.size(1)
-        attn_weights = query_states @ key_states.transpose(1, 2) / math.sqrt(self.head_dim)
-        if attention_mask is not None:
-            attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len)
-            # print(f"{ attention_mask = }")
-            # print(f"{ attention_mask.shape = }")
-            # print(f"{ attn_weights.shape = }")
-            attn_weights = attn_weights.masked_fill_(attention_mask == 0, -1e9)
-        attn_weights = attn_weights.view(bsz * self.num_heads, tgt_len, src_len)
-
-
-        attention_mask = nn.functional.softmax(attn_weights, dim=-1)
+        query_states = query_states.view(query_states.shape[0], query_states.shape[1], self.num_heads,self.head_dim).transpose(1, 2)
+        key_states = key_states.view(key_states.shape[0], key_states.shape[1], self.num_heads,self.head_dim).transpose(1, 2)
+        value_states = value_states.view(value_states.shape[0], value_states.shape[1], self.num_heads,self.head_dim).transpose(1, 2)
 
         if layer_head_mask is not None:
-            attn_weights = layer_head_mask.view(1, -1, 1, 1) * attn_weights.view(bsz, self.num_heads, tgt_len, src_len)
-            attn_weights = attn_weights.view(bsz * self.num_heads, tgt_len, src_len)
+            attn_weights = layer_head_mask.view(1, -1, 1, 1) * attn_weights.view(hidden_states.shape[0], self.num_heads, hidden_states.shape[1], hidden_states.shape[1])
+            attn_weights = attn_weights.view(hidden_states.shape[0] * self.num_heads, hidden_states.shape[1],hidden_states.shape[1])
+            
+        attn_weights = BartAttention._sdpa(query_states, key_states, value_states, attention_mask, self.dropout)
+        attn_weights = attn_weights.transpose(1, 2).contiguous().view(attn_weights.shape[0], -1, self.num_heads * self.head_dim)
 
-        attn_probs = self.dropout(attn_weights)
-        attn_output = attn_probs @ value_states
-
-        attn_output = attn_output.view(bsz, self.num_heads, tgt_len, self.head_dim)
-        attn_output = attn_output.transpose(1, 2)
-        attn_output = attn_output.reshape(bsz, tgt_len, self.embed_dim)
-
-        attn_output = self.out_proj(attn_output)
+        attn_output = self.out_proj(attn_weights)
 
         return attn_output
     
