1fac00778f70579e58e89c3cbdd120147bce7c2b
phamngocthi <pnthi.it.cp@gmail.com>
2024-06-06 09:05:54 +0700

    fix

--- models/scratch/bart_model_from_scratch/classification_head.py
+++ models/scratch/bart_model_from_scratch/classification_head.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
-from .utils.act_fn import (
-    ACT2FN,
-)
+from .utils import ACT_FN
 
 class BartClassificationHead(nn.Module):
     def __init__(
@@ -32,7 +30,7 @@ class BartClassificationHead(nn.Module):
         hidden_states = self.dropout(hidden_states)
         hidden_states = self.dense(hidden_states)
         if self.act_fn is not None:
-            hidden_states = ACT2FN[self.act_fn](hidden_states)
+            hidden_states = ACT_FN[self.act_fn](hidden_states)
         hidden_states = self.dropout(hidden_states)
         hidden_states = self.out(hidden_states)
         return hidden_states
