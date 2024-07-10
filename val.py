390b732ff34d4655d608329eba7b75f2b2239bb2
phamngocthi <pnthi.it.cp@gmail.com>
2024-07-10 23:08:49 +0700

    print token

--- val.py
+++ val.py
@@ -288,7 +288,10 @@ def validate(model, config, beam_size, val_dataloader, num_example=20):
             if config["type_search"] in [BEAM_SEARCH, DIVERSE_BEAM_SEARCH]:
                 pred_ids = preds_ids[0].tgt.squeeze()
             
-            pred_text = tokenizer_tgt.decode(pred_ids.detach().cpu().numpy())
+            pred_text = tokenizer_tgt.decode(
+                pred_ids.detach().cpu().numpy(),
+                skip_special_tokens=True,
+            )
 
             rouge_preds.append(pred_text)
             rouge_targets.append(tgt_text)  
@@ -316,7 +319,10 @@ def validate(model, config, beam_size, val_dataloader, num_example=20):
                 print(f"{f'SOURCE: ':>12}{src_text}")
                 print(f"{f'TARGET: ':>12}{tgt_text}")
                 for i in range(len(preds_ids)):
-                    text = tokenizer_tgt.decode(preds_ids[i].tgt.squeeze().detach().cpu().numpy())
+                    text = tokenizer_tgt.decode(
+                        preds_ids[i].tgt.squeeze().detach().cpu().numpy(),
+                        skip_special_tokens=True,
+                    )
                     print(f"{f'TOKENS TARGET {i}: ':>12}{[tokenizer_tgt.encode(text).tokens]}")
                     print(f"{f'PREDICTED {i}: ':>12}{text}")
                     print()
