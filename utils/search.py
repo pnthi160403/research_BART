c29761a60670ab9c086cdd594b0f13d8439e52e7
phamngocthi <pnthi.it.cp@gmail.com>
2024-07-11 09:36:50 +0700

    fix step and n_gram

--- utils/search.py
+++ utils/search.py
@@ -281,7 +281,7 @@ class DiverseBeamSearch(Search):
                         dim=1,
                     )
                 # N_GRAM_TYPE_DIVERSITY
-                elif self.type_diversity_function is N_GRAM_TYPE_DIVERSITY and step + 1 >= self.n_gram:
+                elif self.type_diversity_function is N_GRAM_TYPE_DIVERSITY and step + 2 >= self.n_gram:
                     # prev_indices_reshape (batch_size, mini_beam_size, num_groups, step + 1)
                     prev_indices_reshape = prev_indices.view(bsz, mini_beam_size, self.num_groups, -1)
                     # prev_indices_cut_n_gram (batch_size, mini_beam_size, num_groups, step + 3 - n_gram, n_gram - 1)
@@ -292,7 +292,6 @@ class DiverseBeamSearch(Search):
                     # last_prev_cut_n_gram_g_gr (batch_size, mini_beam_size, g, n_gram - 1)
                     last_prev_cut_n_gram_g_gr = prev_indices_cut_n_gram[:, :, :g, -1, :]
                     # last_prev_cut_n_gram_g (batch_size, mini_beam_size, n_gram - 1)
-                    # print(f"{ indices_cut_n_gram.shape = }")
                     last_prev_cut_n_gram_g = prev_indices_cut_n_gram[:, :, g, -1, :]
                     # overlap_n_gram (batch_size, mini_beam_size, g, n_gram - 1)
                     overlap_n_gram = (last_prev_cut_n_gram_g.unsqueeze(-2) == last_prev_cut_n_gram_g_gr).int()
@@ -328,7 +327,6 @@ class DiverseBeamSearch(Search):
                         penalty_val = penalty_val.view(bsz, mini_beam_size, -1).contiguous()
                     else:
                         penalty_val = torch.ones(last_indices_g_gr_similarities.size())
-                    # penalty_val = torch.ones(last_indices_g_gr_similarities.size())
 
                     # diversity_buf (batch_size, vocab_size)
                     diversity_buf.scatter_add_(
@@ -348,14 +346,12 @@ class DiverseBeamSearch(Search):
             # scores_buf (batch_size, mini_beam_size)
             # indices_buf (batch_size, mini_beam_size)
             # beams_buf (batch_size, mini_beam_size)
-            # print(f"{ mask_stop_search_g = }")
             scores_buf, indices_buf, beams_buf = self.beam.step(
                 step=step,
                 lprobs=lprobs_g,
                 scores=scores_g,
                 mask_stop_search=mask_stop_search_g,
             )
-            # print(f"{ indices_buf = }")
             prev_indices_buf = prev_indices_g.view(bsz * mini_beam_size, -1).contiguous()[beams_buf.view(-1).contiguous()]
             # (batch_size, mini_beam_size, step + 1)
             prev_indices_buf = prev_indices_buf.view(bsz, mini_beam_size, -1).contiguous()
@@ -380,8 +376,6 @@ class DiverseBeamSearch(Search):
         # interleave results from different groups
         scores_buf = torch.stack(scores_G, dim=2).view(bsz, -1)
         indices_buf = indices[:, :, :, -1].view(bsz, -1)
-        # print(f"{ indices = }")
-        # print(f"{ indices_buf = }")
         beams_buf = torch.stack(beams_G, dim=2).view(bsz, -1)
 
         # find num of overlapped tokens for each group pair
