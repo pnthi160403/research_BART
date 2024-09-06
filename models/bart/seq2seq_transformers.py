5ec81f91d0c5446e7845b21f364051f4f3d89462
Thi Pham <pnthi.it.cp@gmail.com>
2024-09-06 18:06:03 +0700

    update

--- models/bart/seq2seq_transformers.py
+++ models/bart/seq2seq_transformers.py
@@ -37,33 +37,11 @@ class BartSeq2seq(nn.Module):
 
         # config
         self.config = config
-        # encoder_embeds
-        self.inputs_embeds = nn.Embedding(
-            num_embeddings=self.config.vocab_size,
-            embedding_dim=config.d_model,
-            padding_idx=config.pad_token_id,
-        )
-        # decoder_embeds
-        self.decoder_inputs_embeds = nn.Embedding(
-            num_embeddings=self.config.vocab_size,
-            embedding_dim=config.d_model,
-            padding_idx=config.pad_token_id,
-        )
-
         self.bart_model = BartModel(config=self.config)
-        self.out  = nn.Linear(config.d_model, self.config.vocab_size, bias=False)
-
-        # Share embeddings
-        self.inputs_embeds.weight = self.decoder_inputs_embeds.weight
-        self.out.weight = self.inputs_embeds.weight
-
-        # Delete input embeddings
-        del self.bart_model.shared
-        del self.bart_model.encoder.embed_tokens
-        del self.bart_model.decoder.embed_tokens
-
-        # Initialize weights
-        self.inputs_embeds.apply(_init_weights)
+        self.out  = nn.Linear(
+            config.d_model,
+            self.config.vocab_size,
+        )
 
     def forward(
         self,
@@ -73,45 +51,31 @@ class BartSeq2seq(nn.Module):
         labels: torch.Tensor=None,
         input_ids: torch.Tensor=None,
     ):
-        # encoder_head_mask (encoder_layers, encoder_attention_heads)
-        # decoder_head_mask (decoder_layers, decoder_attention_heads)
-        # encoder
-        inputs_embeds = self.inputs_embeds(input_ids)
-        decoder_inputs_embeds = self.decoder_inputs_embeds(decoder_input_ids)
         outputs = self.bart_model(
-            inputs_embeds=inputs_embeds,
             attention_mask=attention_mask,
-            decoder_inputs_embeds=decoder_inputs_embeds,
+            input_ids=input_ids,
+            decoder_input_ids=decoder_input_ids,
             decoder_attention_mask=decoder_attention_mask,
         )
-        logits = self.out(outputs.last_hidden_state)
-
-        if labels is None:
-            return (logits,)
-        
-        if labels is not None:
-            if self.config.pad_token_id is not None:
-                loss_fn = nn.CrossEntropyLoss(
-                    ignore_index=self.config.pad_token_id,
-                )
-            else:
-                loss_fn = nn.CrossEntropyLoss()
-            loss = loss_fn(logits.view(-1, self.config.vocab_size), labels.view(-1))
-            return logits, loss
+        last_hidden_state = outputs.last_hidden_state
+        logits = self.out(last_hidden_state)
+        loss_fn = nn.CrossEntropyLoss(
+            ignore_index=self.config.pad_token_id,
+        )
+        loss = loss_fn(logits.view(-1, self.config.vocab_size), labels.view(-1))
+        return logits, loss
         
     def get_encoder_out(
         self,
         attention_mask: torch.Tensor=None,
         input_ids: torch.Tensor=None,
     ):
-        inputs_embeds = self.inputs_embeds(input_ids)
         outputs = self.bart_model.encoder(
-            inputs_embeds=inputs_embeds,
+            input_ids=input_ids,
             attention_mask=attention_mask,
         )
-        logits = outputs.last_hidden_state
         return BartEncoderSeq2seqOut(
-            logits=logits,
+            logits=outputs.last_hidden_state,
         )
     
     def get_decoder_out(
@@ -125,16 +89,14 @@ class BartSeq2seq(nn.Module):
         use_cache: bool=False,
         pos_idx: int=None,
     ):
-        inputs_embeds = self.decoder_inputs_embeds(input_ids)
         outputs = self.bart_model.decoder(
-            inputs_embeds=inputs_embeds,
+            input_ids=input_ids,
             encoder_hidden_states=encoder_hidden_states,
             attention_mask=attention_mask,
             encoder_attention_mask=encoder_attention_mask,
             past_key_values=past_key_values,
             use_cache=use_cache,
         )
-
         return BartDecoderSeq2seqOut(
             logits=outputs.last_hidden_state,
             past_key_values=outputs.past_key_values,
